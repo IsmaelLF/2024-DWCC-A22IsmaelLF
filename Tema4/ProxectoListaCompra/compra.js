46b71b20a5bbd9d404642bc1f531caef9fa17523
A22IsmaelLF <A22IsmaelLF@iessanclemente.net>
2024-12-01 22:42:49 +0100

    01-12-24

--- Tema4/ProxectoListaCompra/compra.js
+++ Tema4/ProxectoListaCompra/compra.js
@@ -9,13 +9,12 @@ let filtro = document.getElementById("filtro");
 
 // Listener para engadir elemento a lista
 
-// TODO: LOCALSTORAGE, ACTUALIZAR ELEMENTO
 botonEngadir.addEventListener("click", () => {
   let li = document.createElement("li");
   let boton = document.createElement("button");
   boton.setAttribute("type", "button");
   boton.className = "botonX";
-  boton.textContent = "x";
+  boton.textContent = "❌";
   if (input.value !== "") {
     li.textContent = input.value;
     let duplicado = false;
@@ -26,21 +25,80 @@ botonEngadir.addEventListener("click", () => {
     });
     if (duplicado) {
       alert("Xa existe este elemento");
+      input.value = "";
       return;
     }
+
     contedor.appendChild(ul);
     ul.appendChild(li);
     li.appendChild(boton);
+
+    let arrayProductos = [];
+    for (const element of lis) {
+      arrayProductos.push(element.firstChild.textContent);
+    }
+
+    localStorage.setItem("productos", JSON.stringify(arrayProductos));
+
     input.value = "";
+
     if (lis.length > 0) {
       botonBorrarTodo.classList.remove("oculto");
     }
   }
+  // Listener para actualizar
+
+  const liClickHandler = () => {
+    let botonModificar = document.createElement("button");
+    botonModificar.setAttribute("type", "button");
+    botonModificar.textContent = "✏️";
+    botonModificar.style.color = "green";
+    boton.insertAdjacentElement("beforebegin", botonModificar);
+
+    botonModificar.addEventListener("click", () => {
+      li.removeEventListener("click", liClickHandler);
+      botonModificar.classList.add("oculto");
+      let inputModificar = document.createElement("input");
+      inputModificar.setAttribute("type", "text");
+
+      inputModificar.value = li.firstChild.textContent;
+      li.textContent = "";
+      li.appendChild(inputModificar);
+
+      let botonConfirmar = document.createElement("button");
+      botonConfirmar.textContent = "✅";
+      li.appendChild(botonConfirmar);
+
+      // TODO: Poder volver a ter boton de modificar. Tense que actualizar no localStorage tamén. Posibilidad de facelo no mismo li sin crear outro(?)
+      botonConfirmar.addEventListener("click", () => {
+        if (inputModificar.value !== "") {
+          li.remove();
+          let novoLi = document.createElement("li");
+          novoLi.textContent = inputModificar.value;
+          ul.appendChild(novoLi);
+          novoLi.appendChild(botonModificar);
+          novoLi.appendChild(boton);
+        }
+      });
+    });
+  };
+
+  li.addEventListener("click", liClickHandler);
 
   // Listener para borrar elemento da lista
   boton.addEventListener("click", () => {
     if (confirm("Queres eliminar este elemento?")) {
+      let productosArrayParseados = JSON.parse(
+        localStorage.getItem("productos")
+      );
+
+      productosArrayParseados = productosArrayParseados.filter(
+        (element) => element !== li.firstChild.textContent
+      );
+      localStorage.setItem("productos", productosArrayParseados);
+
       li.remove();
+
       if (lis.length < 1) {
         botonBorrarTodo.classList.add("oculto");
       }
