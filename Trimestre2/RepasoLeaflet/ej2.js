4cc6cd315eb08b4e0dde6e747a4088e051042203
IsmaelLF <114486744+IsmaelLF@users.noreply.github.com>
2025-02-04 18:14:37 +0100

    aa

--- Trimestre2/RepasoLeaflet/ej2.js
+++ Trimestre2/RepasoLeaflet/ej2.js
@@ -2,7 +2,13 @@
 const listado = document.getElementById("listado");
 let formInput = document.createElement("input");
 let botonSubmit = document.createElement("button");
+formInput.classList.add("oculto");
+botonSubmit.classList.add("oculto");
+listado.appendChild(formInput);
+listado.appendChild(botonSubmit);
 let marker = null;
+let arrayMarkers = [];
+let tooltip = L.tooltip();
 const map = L.map("map", {
   center: [42.8802049, -8.5447697, 13],
   zoom: 13,
@@ -15,12 +21,36 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 function mapClick(e) {
   marker = L.marker(e.latlng, { draggable: true }).addTo(map);
   botonSubmit.textContent = "Engadir";
-  listado.appendChild(formInput);
-  listado.appendChild(botonSubmit);
+  formInput.classList.remove("oculto");
+  botonSubmit.classList.remove("oculto");
   // Poñer o foco unha vez creado
   formInput.focus();
-  formInput.addEventListener("input", (e) => {
-    console.log(formInput.value);
-  });
 }
+formInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    marker.bindTooltip(`${formInput.value}`).openTooltip();
+    arrayMarkers.push(marker["_latlng"]);
+    let li = document.createElement("li");
+    li.textContent = formInput.value;
+    listado.appendChild(li);
+    localStorage.setItem(
+      `${formInput.value}`,
+      JSON.stringify(marker["_latlng"])
+    );
+    formInput.value = "";
+    formInput.classList.add("oculto");
+    botonSubmit.classList.add("oculto");
+  }
+});
+listado.addEventListener("click", (e) => {
+  console.log(localStorage);
+
+  // if (localStorage.includes(e.target.closest("li").textContent)) {
+  //   console.log("aaaa");
+  // }
+});
+
+listado.addEventListener("mouseover", () => {
+  listado.style.cursor = "pointer";
+});
 map.on("click", mapClick);
