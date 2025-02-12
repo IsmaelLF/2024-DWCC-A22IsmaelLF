732bebf4efca7dc700661a264e5c58e23b316d7e
A22IsmaelLF <A22IsmaelLF@iessanclemente.net>
2025-02-12 14:53:50 +0100

    x

--- Trimestre2/Tema2/Vue/Ej3/script.js
+++ Trimestre2/Tema2/Vue/Ej3/script.js
@@ -8,7 +8,21 @@ import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 createApp({
   data() {
-    return {};
+    return {
+      texto: "",
+      message: "",
+      picked: "",
+      checked: "",
+      selected: "",
+    };
   },
-  methods: {},
+  methods: {
+    resetForm() {
+      this.texto = "";
+      this.message = "";
+      this.picked = "";
+      this.checked = "";
+      this.selected = "";
+    },
+  },
 }).mount("#app");
