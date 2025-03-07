fb3019d812fc2dbed4a4446976bf4c84aa51416a
IsmaelLF <114486744+IsmaelLF@users.noreply.github.com>
2025-03-07 17:25:53 +0100

    Components - Page 18

---
+++ Trimestre2/Tema2/VueComponents/PropsEj1/src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import Contact from "./components/Contact.vue";
+
+export default {
+  components: {
+    Contact,
+  },
+  data() {
+    return {
+      persoas: [
+        {
+          nome: "Ada Lovelace",
+          telefono: 123456789,
+          correo: "ada@lovelace.edu",
+        },
+        {
+          nome: "Lovelace Ada",
+          telefono: 987654321,
+          correo: "lovelace@ada.edu",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Contact
+      v-for="(persoa, index) in persoas"
+      :key="index"
+      :nome="persoa.nome"
+      :telefono="persoa.telefono"
+      :correo="persoa.correo"
+    />
+  </div>
+</template>
---
+++ Trimestre2/Tema2/VueComponents/PropsEj1/src/components/Contact.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  props: {
+    nome: {
+      type: String,
+      required: true,
+    },
+    telefono: {
+      type: Number,
+      required: true,
+    },
+    correo: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isOculto: true,
+    };
+  },
+  methods: {
+    mostrarInfo() {
+      this.isOculto = !this.isOculto;
+    },
+  },
+  computed: {
+    textoBoton() {
+      return this.isOculto ? "Mostrar Información" : "Ocultar Información";
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h3>{{ nome }}</h3>
+    <button @click="mostrarInfo">{{ textoBoton }}</button>
+    <div :class="{ oculto: isOculto }">
+      <h4>{{ nome }}</h4>
+      <h4>{{ telefono }}</h4>
+      <h4>{{ correo }}</h4>
+    </div>
+  </div>
+</template>
+
+<style>
+.oculto {
+  display: none;
+}
+</style>
