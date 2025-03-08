d55bc80426430e13913552c5443654c456ef3897
IsmaelLF <114486744+IsmaelLF@users.noreply.github.com>
2025-03-08 13:10:59 +0100

    propsej2

---
+++ Trimestre2/Tema2/VueComponents/PropsEj2/src/App.vue
@@ -0,0 +1,12 @@
+<script>
+import MostrarNomeAno from "./components/MostrarNomeAno.vue";
+import FormularioDatos from "./components/FormularioDatos.vue";
+
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<template></template>
+<style></style>
